f0dd9b08f48ecbf9c01d7f2fc05457957c258688
umarhidaayat <umarhidaayat@gmail.com>
2026-08-28 18:54:38 +0800

    Perbaikan Logo tabulasi

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $company->name ?? 'BMEX Money Changer' }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('bmex.png') }}?v=1.0">
+    <link rel="apple-touch-icon" href="{{ asset('bmex.png') }}?v=1.0">
     
     <!-- Tailwind CDN untuk memastikan CSS termuat sempurna -->
     <script src="https://cdn.tailwindcss.com"></script>
